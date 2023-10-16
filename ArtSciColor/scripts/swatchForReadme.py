b65f1a18e3d9246a5c245aaddd80486a7c959d04
chipdelmal <chipdelmal@gmail.com>
2023-10-16 11:44:26 -0700

    New test on readme swatches

--- ArtSciColor/scripts/swatchForReadme.py
+++ ArtSciColor/scripts/swatchForReadme.py
@@ -13,7 +13,7 @@ if art.isNotebook():
     CATEGORY = 'Gaming'
 else:
     ARTIST = argv[1]
-DIMS = (20, 1000)
+DIMS = (500, 250)
 ###############################################################################
 # Setup Paths
 ###############################################################################
@@ -39,7 +39,7 @@ for ix in range(db.shape[0]):
     swatch = db.iloc[ix]['palette']
     hexSwt = [Color(c) for c in swatch.split(',')]
     dimg = np.zeros((DIMS[0], DIMS[1], 3))
-    swatch = art.genColorSwatch(dimg, 1000, hexSwt, proportionalHeight=False)
+    swatch = art.genColorSwatch(dimg, DIMS[1], hexSwt, proportionalHeight=False)
     swatches.append(swatch)
 fullSwatch =np.transpose(np.vstack(swatches), axes=(1, 0, 2))
 imgOut = Image.fromarray(fullSwatch.astype('uint8'), 'RGB')
